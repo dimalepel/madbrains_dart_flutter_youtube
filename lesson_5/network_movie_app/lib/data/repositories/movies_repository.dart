fdb2857a4e2031edeceb30e72adad52c9368d5bc
Dimitry Vashkevich <dimalepel@mail.ru>
2023-11-23 17:50:17 +0300

    feat: update repository

--- lesson_5/network_movie_app/lib/data/repositories/movies_repository.dart
+++ lesson_5/network_movie_app/lib/data/repositories/movies_repository.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:network_movie_app/components/constants.dart';
+import 'package:network_movie_app/data/mappers/home_mapper.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../../domain/models/home_model.dart';
+import '../dtos/home_dto.dart';
 
 class MoviesRepository {
   static final Dio _dio = Dio()
@@ -16,9 +18,19 @@ class MoviesRepository {
 
   static Future<HomeModel?> loadData() async {
     try {
-      await _dio.get(MovieQuery.mockUrl);
-    } catch (e) {
-      print(e);
+      const String url = '${MovieQuery.mockUrl}';
+      final Response<dynamic> response = await _dio.get<Map<dynamic, dynamic>>(
+        url,
+      );
+
+      final HomeDTO dto =
+          HomeDTO.fromJson(response.data as Map<String, dynamic>);
+      final HomeModel model = dto.toDomain();
+
+      return model;
+    } on DioException catch (error) {
+      print(error.response?.statusCode);
+      return null;
     }
   }
 }
